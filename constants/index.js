b69d98f75f3b2145d207354c2c4f9df78c1751bf
NurjahonErgashevMe <140044620+NurjahonErgashevMe@users.noreply.github.com>
2024-06-10 18:50:13 +0500

    changed github name

--- constants/index.js
+++ constants/index.js
@@ -10,5 +10,5 @@ export const TELEGRAM_URL = "https://t.me/NurjahonErgashevMe";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/nurjahon-ergashev-me/";
 export const LINKEDIN_NAME = "Nurjahon Ergashev";
 
-export const GITHUB_NAME = "Nurjahon Ergashev";
+export const GITHUB_NAME = "@NurjahonErgashevMe";
 export const GITHUB_URL = "https://github.com/NurjahonErgashevMe";
